5225b04b2a028182006f51cff7341c3b5b4f50d0
Vinicius Marinho <viniciusmarinho1616@gmail.com>
2020-12-14 19:08:47 -0300

    Criando sistema de respotas

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ const connection = require('./database/database');
 // Chama o Model Pergunta
 const Pergunta = require('./database/Pergunta');
 
+// Chama o Model Resposta
+const Resposta = require('./database/Resposta');
+
 // Testando a conexão
 connection
     .authenticate() // Autentica a conexão 
@@ -52,6 +55,44 @@ app.post('/salvarpergunta', (req,res) =>{
         descricao: descricao,
     }).then(() =>{ // Verifica se a inserção ocorreu com sucesso
         res.redirect('/');
+    });
+});
+
+app.get('/pergunta/:id', (req,res) => {
+    var id = req.params.id;
+
+    Pergunta.findOne({  // Busca apenas um dado na tabela do banco 
+        where: {id: id}
+    }).then(pergunta => {
+        if (pergunta != undefined) {
+
+            Resposta.findAll({
+                where: {perguntId: pergunta.id},
+                order: [
+                    ['id','DESC']
+                ]
+            }).then(respostas => {
+                res.render('pergunta', {
+                    pergunta: pergunta,
+                    respostas: respostas,
+                });
+            });
+
+           
+        }else{
+            res.redirect('/');
+        }
+    });
+});
+
+app.post('/responder', (req,res) => {
+    var corpo = req.body.corpo;
+    var perguntaId = req.body.pergunta;
+    Resposta.create({
+        corpo: corpo,
+        perguntId: perguntaId,
+    }).then(() =>{ // Verifica se a inserção ocorreu com sucesso
+        res.redirect('/pergunta/'+perguntaId);
     });
 });
 
